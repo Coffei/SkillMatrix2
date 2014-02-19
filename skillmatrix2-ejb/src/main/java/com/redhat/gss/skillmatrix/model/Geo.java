4e8985f93008db7a115c35c0151dbe6bed60c919
Jonáš Trantina <jtrantin@redhat.com>
2014-02-19 13:23:53 +0100

    Some changes due to Postgres migration

--- skillmatrix2-ejb/src/main/java/com/redhat/gss/skillmatrix/model/Geo.java
+++ skillmatrix2-ejb/src/main/java/com/redhat/gss/skillmatrix/model/Geo.java
@@ -5,10 +5,10 @@ import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
- * Entity class representing a Geographic location. It is represented by a code of the geo (e.g. EMEA, NA/SA) and the time zone offset.
- * User: jtrantin
- * Date: 7/16/13
- * Time: 10:05 AM
+ * Entity class representing a Geographic location. It is represented by a code of the geo (e.g. EMEA, NA/SA) and the time zone offsetValue.
+ User: jtrantin
+ Date: 7/16/13
+ Time: 10:05 AM
  * @see GeoEnum
  */
 @Entity
@@ -16,7 +16,7 @@ public class Geo implements Serializable {
     public Geo() {}
     public Geo(GeoEnum geocode, int offset) {
         this.geocode = geocode;
-        this.offset = offset;
+        this.offsetValue = offset;
     }
 
     @Id
@@ -26,7 +26,7 @@ public class Geo implements Serializable {
     @Enumerated(EnumType.STRING)
     private GeoEnum geocode;
 
-    private int offset;
+    private int offsetValue;
 
     public GeoEnum getGeocode() {
         return geocode;
@@ -37,15 +37,15 @@ public class Geo implements Serializable {
     }
 
     public int getOffset() {
-        return offset;
+        return offsetValue;
     }
 
     public void setOffset(int offset) {
-        this.offset = offset;
+        this.offsetValue = offset;
     }
 
     @Override
-    // compares ids, if ids are null, it compares geocode and offset
+    // compares ids, if ids are null, it compares geocode and offsetValue
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -59,7 +59,7 @@ public class Geo implements Serializable {
             if(geocode==null? geo.geocode!=null : !geocode.equals(geo.geocode))
                 return false;
 
-            if(offset!=geo.offset)
+            if(offsetValue!=geo.offsetValue)
                 return false;
 
         } else if (!id.equals(geo.id))
@@ -73,7 +73,7 @@ public class Geo implements Serializable {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null) ? (geocode==null? 0 : geocode.hashCode() * (offset==0? 1 : offset)) : id.hashCode());
+        result = prime * result + ((id == null) ? (geocode==null? 0 : geocode.hashCode() * (offsetValue==0? 1 : offsetValue)) : id.hashCode());
         return result;
     }
 }
